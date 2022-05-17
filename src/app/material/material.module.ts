b4ecd7f7444555fe078042ef6f38d116748eb3d0
robertocajueiro <beto.cajueiro@gmail.com>
2022-05-17 00:03:58 -0300

    grid

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,6 +9,7 @@ import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list'
 import { MatDividerModule } from  '@angular/material/divider'
+import { MatGridListModule } from '@angular/material/grid-list'
 
 
 const material = [
@@ -22,7 +23,8 @@ const material = [
   MatSidenavModule,
   MatMenuModule,
   MatListModule,
-  MatDividerModule
+  MatDividerModule,
+  MatGridListModule
 ];
 
 
